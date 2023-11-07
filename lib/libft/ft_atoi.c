49d38dcad9facf219fb3e85adbf1bcb9be553476
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-11-07 16:51:26 +0100

    add libft library

---
+++ lib/libft/ft_atoi.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/26 18:20:39 by zelhajou          #+#    #+#             */
+/*   Updated: 2023/10/22 10:13:41 by zelhajou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+	ft_atoi() convert a string to an integer
+*/
+
+#include "libft.h"
+
+int	ft_atoi(const char *s)
+{
+	int	sign;
+	int	nb;
+	int	n;
+
+	nb = 0;
+	sign = 1;
+	if (*s == '+')
+		s++;
+	if (*s == '-' && s++)
+		sign = -1;
+	while (*s)
+	{
+		n = (*s - 48) * sign;
+		if (nb > (INT_MAX / 10) || (nb == (INT_MAX / 10) && n > 7))
+			ft_error(1);
+		if (nb < (INT_MIN / 10) || (nb == (INT_MIN / 10) && n < -8))
+			ft_error(1);
+		nb = nb * 10 + n;
+		s++;
+	}
+	return (nb);
+}
---
+++ lib/libft/ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/06 01:03:10 by zelhajou          #+#    #+#             */
+/*   Updated: 2022/11/21 02:54:27 by zelhajou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	int		i;
+	char	*str;
+
+	if (!s || !f)
+		return (0);
+	i = 0;
+	str = malloc(sizeof(char) * ft_strlen(s) + 1);
+	if (!str)
+		return (NULL);
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
