1b64e2e57592257756d1c9dff2a38132c51ba9d6
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-11-15 16:59:38 +0100

    Update client.c

--- src/client/client.c
+++ src/client/client.c
@@ -6,13 +6,28 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 17:23:19 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/11/07 17:37:41 by zelhajou         ###   ########.fr       */
+/*   Updated: 2023/11/14 10:08:03 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "utils.h"
+#include "minitalk.h"
 
-int main(int argc, char **argv) {
-    printf("hehehe");
-    return 0;
+
+int	main(int argc, char **argv) {
+	pid_t	pid;
+	char	*str;
+
+	if (argc != 3)
+	{
+		ft_putstr_fd("Usage: ./client [PID] [string]\n", 2);
+		exit(1);
+	}
+	pid = ft_atoi(argv[1]);
+	if (pid <= 0) {
+        ft_putstr_fd("Invalid PID ", 2);
+		ft_putstr_fd(argv[1], 2);
+		ft_putstr_fd("\n", 2);
+		exit(1);
+	}
+    return (0);
 }
