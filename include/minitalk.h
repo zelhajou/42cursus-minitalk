2cf5c16c42c7ca489968a9b32d66b8e2c7f60e9d
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-11-19 19:11:51 +0100

    Update minitalk.h

--- include/minitalk.h
+++ include/minitalk.h
@@ -5,22 +5,19 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/07 17:06:19 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/11/18 13:43:02 by zelhajou         ###   ########.fr       */
+/*   Created: 2023/11/19 18:07:48 by zelhajou          #+#    #+#             */
+/*   Updated: 2023/11/19 18:08:59 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef UTILS_H
-#define UTILS_H
+#ifndef MINITALK_H
+# define MINITALK_H
 
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
 # include <signal.h>
-
-
 # include "libft.h"
 # include "ft_printf.h"
-
 
 #endif
