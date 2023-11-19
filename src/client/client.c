4f8af0000831d6bb3b56c38a93e5c9ff946b0fdd
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-11-19 19:20:29 +0100

    Update client.c

--- src/client/client.c
+++ src/client/client.c
@@ -6,65 +6,64 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 17:23:19 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/11/18 18:50:44 by zelhajou         ###   ########.fr       */
+/*   Updated: 2023/11/19 18:15:08 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	ft_send_bit(int pid; int bit)
+void	ft_send_bit(int pid, int bit)
 {
-	int signal;
+	int	signal;
+
 	if (bit == 1)
-		signal = SIGUSR2;
-	else
 		signal = SIGUSR1;
+	else
+		signal = SIGUSR2;
 	if (kill(pid, signal) == -1)
 	{
-		ft_putstr_fd("Error");
-		exit(1);
+		ft_putstr_fd("Error", 2);
+		exit(EXIT_FAILURE);
 	}
 }
 
 void	ft_send_char(int pid, unsigned char c)
 {
-	int i;
+	int	i;
 
-	i = 0;
-	while (i < 8)
+	i = 7;
+	while (i >= 0)
 	{
-		send_bit(pid, (c >> i) & 1);
+		ft_send_bit(pid, (c >> i) & 1);
 		usleep(100);
-		i++;
+		i--;
 	}
 }
 
 void	ft_send_string(int pid, const char *str)
 {
 	while (*str)
-		send_char(pid, *str++);
-	send_char(pid, '\0');
+		ft_send_char(pid, *str++);
+	ft_send_char(pid, '\0');
 }
 
 int	main(int argc, char **argv)
 {
 	pid_t	pid;
-	char	*str;
 
 	if (argc != 3)
 	{
 		ft_putstr_fd("Usage: ./client [PID] [string]\n", 2);
-		return (1);
+		exit(EXIT_FAILURE);
 	}
 	pid = ft_atoi(argv[1]);
-	if (pid <= 0) {
-		ft_putstr_fd("Invalid PID :", 2);
+	if (pid <= 0)
+	{
+		ft_putstr_fd("Invalid PID : ", 2);
 		ft_putstr_fd(argv[1], 2);
 		ft_putstr_fd("\n", 2);
-		return (1);
+		exit(EXIT_FAILURE);
 	}
-
-	ft_send_string(pid, argv[1]);
-
+	ft_send_string(pid, argv[2]);
 	return (0);
 }
