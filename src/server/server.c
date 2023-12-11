4b2b1ca22d7221f34cc18e64b61006bcd2738c05
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-12-11 10:39:56 +0100

    Update server.c

--- src/server/server.c
+++ src/server/server.c
@@ -6,25 +6,11 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 17:24:10 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/12/09 11:03:27 by zelhajou         ###   ########.fr       */
+/*   Updated: 2023/12/11 10:27:42 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-
-int	main(void)
-{
-	struct sigaction	sa;
-
-	ft_printf("Server PID: %d\n", getpid());
-	sa.sa_handler = &ft_handle_signal;
-	sa.sa_flags = SA_RESTART;
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
-	while (1)
-		pause();
-	return (0);
-}
 
 void	ft_handle_signal(int signum)
 {
@@ -41,4 +27,18 @@ void	ft_handle_signal(int signum)
 		bit_count = 0;
 		character = 0;
 	}
+}
+
+int	main(void)
+{
+	struct sigaction	sa;
+
+	ft_printf("Server PID: %d\n", getpid());
+	sa.sa_handler = &ft_handle_signal;
+	sa.sa_flags = SA_RESTART;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+		pause();
+	return (0);
 }
